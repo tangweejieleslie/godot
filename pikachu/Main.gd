af757aa4485eeaf20529e1b856f5d0daf5f9e558
Leslie Tang <tangweejieleslie+github@gmail.com>
2019-09-17 16:02:02 +0800

    WIP: fixing random pokeball missing

--- pikachu/Main.gd
+++ pikachu/Main.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
 var score = 0
-const MIN = 30
+const MIN = 80
 const MAX_WIDTH = 600-MIN
 const MAX_HEGHT = 1024-MIN
 const MAXSPEED = 750
 
-
-func _ready():
-	pass # Replace with function body.
 
 func handle_pokeball_contact(contactWith):
 	print(contactWith," hit pokeball")
@@ -21,11 +18,10 @@ func handle_pokeball_contact(contactWith):
 
 func _on_Item_area_entered(area):
 	handle_pokeball_contact(area)
-	pass # Replace with function body.
+	area.free()
 
 func _on_Item_body_entered(body):
 	handle_pokeball_contact(body)
-	pass # Replace with function body.
 	
 func random_number_generator(MIN, MAX):
 	randomize()
@@ -43,14 +39,12 @@ func increase_speed(score):
 func _on_KinematicBody2D_attack():
 	print("Received Attack Signal")
 	var new_projectile = load("res://Area2D_projectile.tscn").instance()
-	print(new_projectile)
+	#print(new_projectile)
 	new_projectile.position = $KinematicBody2D.position
+	if Input.is_action_pressed("ui_left"):
+		new_projectile.speed = -new_projectile.speed 
 	add_child(new_projectile)
 	
-	pass # Replace with function body.
-
 func _on_Walls_area_entered(area):
-	print(area)
 	print(area, "has entered")
 	area.free() # This prevents clogging up with infinite instance of electro ball
-	pass # Replace with function body.
